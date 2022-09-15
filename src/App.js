b2bfcc7c909114b6b0826484dffc278af3dec42e
b00mshaka1aka <shishkinmihail961@gmail.com>
2022-09-15 15:48:17 +0300

    New conception

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-
-import Header from './components/Header';
-import Home from './pages/Home';
-
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Home />
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
